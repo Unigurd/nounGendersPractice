9288e87eb6d1247ed87a2d9e5d22d669f5f378ec
Sigurd Dam Sonniks <lsn777@alumni.ku.dk>
2019-11-18 22:59:18 +0100

    Okay, NU parses der ordentligt!

--- nounGenders.hs
+++ nounGenders.hs
@@ -5,13 +5,14 @@ import qualified Data.Text.IO as TIO
 import Numeric.Natural
 import Text.Read
 
-data Gender = Der | Die | Das
-data Growth = Lin | Exp Natural
+data Gender = Der | Die | Das deriving Show
+data Growth = Lin | Exp Natural deriving Show
 
 data Word = Word {word :: T.Text,
                   gender :: Gender,
                   value :: Natural,
-                  growth :: Growth}
+                  growth :: Growth} 
+                  deriving Show
 
 data Tree a = 
   Branch (Tree a) (Tree a) a -- invariant: the a is the sum of the a's in the subtrees
@@ -47,18 +48,21 @@ createWord word value growth gender =
 parseWord [genderStr, word] = (createWord word 0 Lin) <$> parseGender genderStr
 parseWord [genderStr, word, valueStr] = do
   gender <- parseGender genderStr 
-  value  <- tReadEither ("Parse error in file: invalid number" `T.append` valueStr) valueStr
+  value  <- tReadEither ("Parse error in file: invalid number: " `T.append` valueStr) valueStr
   return $ createWord word value Lin gender
 parseWord [genderStr, word, valueStr, growthStr] = do
   gender <- parseGender genderStr 
   value  <- tReadEither ("Parse error in file: invalid value number: "  `T.append` valueStr) valueStr
   growth <- parseGrowth ("Parse error in file: invalid growth number: " `T.append` growthStr) growthStr
   return $ createWord word value Lin gender
+parseWord _ = Left "Parse error in file: Haven't bothered to write good enough error messages for me to tell you what"
+
+
 
 main = do
   fileText <- TIO.readFile "data.txt"
-  let input = removeEmpties  $ removeComments <$> T.words <$> (T.lines fileText)
+  let input = sequence $ fmap parseWord $ removeEmpties  $ removeComments <$> T.words <$> (T.lines fileText)
 --((fmap T.words) . T.lines) <$> TIO.readFile "data.txt"
-  putStr (show input)
+  putStrLn (show input)
   --sequence2 (fmap2 TIO.putStr input)
 --((`T.append` "\n") <$> input))
