e0505e4310d9ad963c2728e38385a7662003de6e
Sigurd Dam Sonniks <lsn777@alumni.ku.dk>
2019-11-22 20:18:31 +0100

    Working on generating random Naturals. Having some trouble.

--- nounGenders.hs
+++ nounGenders.hs
@@ -5,8 +5,52 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import Numeric.Natural
 import Text.Read
-import Data.List.NonEmpty
+import Data.List.NonEmpty as NE
 import Data.Function
+import System.Random
+
+-- Copied from 
+-- https://hackage.haskell.org/package/random-1.1/docs/src/System.Random.html#randomIvalIntegral
+-- The two integer functions below take an [inclusive,inclusive] range.
+randomIvalIntegral :: (RandomGen g, Integral a) => (a, a) -> g -> (a, g)
+randomIvalIntegral (l,h) = randomIvalInteger (toInteger l, toInteger h)
+
+randomIvalInteger :: (RandomGen g, Num a) => (Integer, Integer) -> g -> (a, g)
+randomIvalInteger (l,h) rng
+ | l > h     = randomIvalInteger (h,l) rng
+ | otherwise = case (f 1 0 rng) of (v, rng') -> (fromInteger (l + v `mod` k), rng')
+     where
+       (genlo, genhi) = genRange rng
+       b = fromIntegral genhi - fromIntegral genlo + 1
+
+       -- Probabilities of the most likely and least likely result
+       -- will differ at most by a factor of (1 +- 1/q).  Assuming the RandomGen
+       -- is uniform, of course
+
+       -- On average, log q / log b more random values will be generated
+       -- than the minimum
+       q = 1000
+       k = h - l + 1
+       magtgt = k * q
+
+       -- generate random values until we exceed the target magnitude 
+       f mag v g | mag >= magtgt = (v, g)
+                 | otherwise = v' `seq`f (mag*b) v' g' where
+                        (x,g') = next g
+                        v' = (v * b + (fromIntegral x - fromIntegral genlo))
+
+integerToNatural :: Integer -> Natural
+integerToNatural i
+  | i < 0 = fromInteger (abs i) - 1
+  | True  = fromInteger i
+integralToNatural = integerToNatural . toInteger
+
+instance Random Natural where
+  randomR (a,b) g = (nat,g') where
+    (int,g') = randomIvalIntegral (a::Natural, b) g
+    nat = integralToNatural int
+    
+  random = randomR (0, (integralToNatural (maxBound::Int)))
 
 data Gender = Der | Die | Das deriving Show
 data Growth = Lin | Exp Natural deriving Show
@@ -16,8 +60,6 @@ data Word = Word {word :: T.Text,
                   value :: Natural,
                   growth :: Growth} 
                   deriving Show
-
-
 
 data Tree a = 
   Branch (Tree a) Natural (Tree a)
@@ -94,13 +136,19 @@ pairUp (a:|(b:restHead:restTail)) =
   (Branch a ((treeVal a) + (treeVal b)) b) `cons` (pairUp (restHead:|restTail))
 pairUp (a:|[])        = a:|[]
 
+rotate :: NonEmpty a -> NonEmpty a
+rotate list = (NE.last list):|(NE.init list)
+
+-- rotate makes sure that no single leaf  can become the right child of the root
+-- (Except when the 3 <= the number of elements <= 6
+-- Otherwise the following could happen:
+-- For example, (a b c d e) ->> (ab cd e) ->> (abcd e)
 buildTree :: NonEmpty (Tree Word) -> Tree Word
 buildTree (result:|[]) = result
-buildTree list         = buildTree $ pairUp list
+buildTree list         = buildTree $ rotate $ pairUp list
 
 values :: Functor f => f Word -> f Natural
 values = fmap value
-
 
 pickWord :: Tree a -> Natural -> a
 pickWord (Leaf word _) _ = word
@@ -111,15 +159,12 @@ pickWord (Branch left _ right) searchVal =
 
 main = do
   fileText <- TIO.readFile "data.txt"
-  let tree = do
+  stdGen <- getStdGen
+  let comp = do
       input <- parseInput fileText
       let leaves = leafify input 
       let tree = buildTree leaves
-      return $ pickWord tree 9
-      --return tree
-      
-  putStrLn (show tree)
-
-
-
--- Right (("Das" :| ["M\228dchen"]) :| ["Der" :| ["Beweis"],"Die" :| ["Polizei"],"Das" :| ["Gesicht","3"],"Das" :| ["Publikum","5"],"Die" :| ["Woche","7","2"],"Der" :| ["Eindruck","5","2"],"Der" :| ["EinDruck","5","3","2"]])
+      let (index,_) = randomR (1, treeVal tree) stdGen
+      let word = pickWord tree $ index
+      return index
+  putStrLn (show comp)
